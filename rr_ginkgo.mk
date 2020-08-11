ed755980a5bbe17e3e37c653432d762504778b35
Rifky Mcz <32016630+rifkymcz@users.noreply.github.com>
2020-08-11 23:59:09 +0800

    RRify

--- rr_ginkgo.mk
+++ rr_ginkgo.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 $(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -25,3 +25,4 @@ PRODUCT_MODEL := Redmi Note 8
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_FACE_UNLOCK_SUPPORTED := true
